14e499e5445e64e185a87fbbf0552fb14ea52eac
Lucas Runkel <lucasrunkel17@gmail.com>
2018-02-10 19:53:59 -0500

    Adding climber subsystem.

---
+++ src/team4391/robot/subsystems/Climb.java
@@ -0,0 +1,21 @@
+package team4391.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Climb extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+	// Setup Talons here
+	
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
